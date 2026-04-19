89ad545d62d114de0c1bd052d088ea1812b0cbf2
(Ariel <ariel.calzada@gmail.com>
2026-04-19 05:18:31 -0500

    Base django project skeleton

---
+++ config/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, reverse_lazy
+from django.views.generic.base import RedirectView
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(url=reverse_lazy("admin:index"))),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
